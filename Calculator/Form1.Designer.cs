1c28b98fd50bcc0eec1a676209e9969de26d9aa2
LouiseDomingo <louisedomingo.edu@yahoo.com>
2021-05-05 20:34:50 +0800

    Encoding for number buttons, decimal button and all clear button.

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -36,18 +36,18 @@ namespace Calculator
             this.unoBtn = new System.Windows.Forms.Button();
             this.dosBtn = new System.Windows.Forms.Button();
             this.tresBtn = new System.Windows.Forms.Button();
-            this.addBtn = new System.Windows.Forms.Button();
             this.kwatroBtn = new System.Windows.Forms.Button();
             this.singkoBtn = new System.Windows.Forms.Button();
             this.saisBtn = new System.Windows.Forms.Button();
-            this.subtractBtn = new System.Windows.Forms.Button();
             this.siyeteBtn = new System.Windows.Forms.Button();
             this.otchoBtn = new System.Windows.Forms.Button();
             this.nuwebeBtn = new System.Windows.Forms.Button();
-            this.multiplyBtn = new System.Windows.Forms.Button();
             this.deleteBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
             this.percentBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.subtractBtn = new System.Windows.Forms.Button();
+            this.multiplyBtn = new System.Windows.Forms.Button();
             this.divideBtn = new System.Windows.Forms.Button();
             this.programScreen = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -62,6 +62,7 @@ namespace Calculator
             this.posNegBtn.TabIndex = 0;
             this.posNegBtn.Text = "+/-";
             this.posNegBtn.UseVisualStyleBackColor = false;
+            this.posNegBtn.Click += new System.EventHandler(this.posNegBtn_Click);
             // 
             // zeroBtn
             // 
@@ -72,6 +73,7 @@ namespace Calculator
             this.zeroBtn.TabIndex = 1;
             this.zeroBtn.Text = "0";
             this.zeroBtn.UseVisualStyleBackColor = true;
+            this.zeroBtn.Click += new System.EventHandler(this.zeroBtn_Click);
             // 
             // decimalBtn
             // 
@@ -83,6 +85,7 @@ namespace Calculator
             this.decimalBtn.TabIndex = 2;
             this.decimalBtn.Text = ".";
             this.decimalBtn.UseVisualStyleBackColor = false;
+            this.decimalBtn.Click += new System.EventHandler(this.decimalBtn_Click);
             // 
             // equalBtn
             // 
@@ -104,6 +107,7 @@ namespace Calculator
             this.unoBtn.TabIndex = 4;
             this.unoBtn.Text = "1";
             this.unoBtn.UseVisualStyleBackColor = true;
+            this.unoBtn.Click += new System.EventHandler(this.unoBtn_Click);
             // 
             // dosBtn
             // 
@@ -114,6 +118,7 @@ namespace Calculator
             this.dosBtn.TabIndex = 5;
             this.dosBtn.Text = "2";
             this.dosBtn.UseVisualStyleBackColor = true;
+            this.dosBtn.Click += new System.EventHandler(this.dosBtn_Click);
             // 
             // tresBtn
             // 
@@ -124,18 +129,7 @@ namespace Calculator
             this.tresBtn.TabIndex = 6;
             this.tresBtn.Text = "3";
             this.tresBtn.UseVisualStyleBackColor = true;
-            // 
-            // addBtn
-            // 
-            this.addBtn.BackColor = System.Drawing.Color.PowderBlue;
-            this.addBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.addBtn.Location = new System.Drawing.Point(210, 229);
-            this.addBtn.Name = "addBtn";
-            this.addBtn.Size = new System.Drawing.Size(60, 38);
-            this.addBtn.TabIndex = 7;
-            this.addBtn.Text = "+";
-            this.addBtn.UseVisualStyleBackColor = false;
-            this.addBtn.Click += new System.EventHandler(this.button8_Click);
+            this.tresBtn.Click += new System.EventHandler(this.tresBtn_Click);
             // 
             // kwatroBtn
             // 
@@ -146,6 +140,7 @@ namespace Calculator
             this.kwatroBtn.TabIndex = 8;
             this.kwatroBtn.Text = "4";
             this.kwatroBtn.UseVisualStyleBackColor = true;
+            this.kwatroBtn.Click += new System.EventHandler(this.kwatroBtn_Click);
             // 
             // singkoBtn
             // 
@@ -156,6 +151,7 @@ namespace Calculator
             this.singkoBtn.TabIndex = 9;
             this.singkoBtn.Text = "5";
             this.singkoBtn.UseVisualStyleBackColor = true;
+            this.singkoBtn.Click += new System.EventHandler(this.singkoBtn_Click);
             // 
             // saisBtn
             // 
@@ -166,17 +162,7 @@ namespace Calculator
             this.saisBtn.TabIndex = 10;
             this.saisBtn.Text = "6";
             this.saisBtn.UseVisualStyleBackColor = true;
-            // 
-            // subtractBtn
-            // 
-            this.subtractBtn.BackColor = System.Drawing.Color.PowderBlue;
-            this.subtractBtn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.subtractBtn.Location = new System.Drawing.Point(210, 185);
-            this.subtractBtn.Name = "subtractBtn";
-            this.subtractBtn.Size = new System.Drawing.Size(60, 38);
-            this.subtractBtn.TabIndex = 11;
-            this.subtractBtn.Text = "-";
-            this.subtractBtn.UseVisualStyleBackColor = false;
+            this.saisBtn.Click += new System.EventHandler(this.saisBtn_Click);
             // 
             // siyeteBtn
             // 
@@ -187,6 +173,7 @@ namespace Calculator
             this.siyeteBtn.TabIndex = 12;
             this.siyeteBtn.Text = "7";
             this.siyeteBtn.UseVisualStyleBackColor = true;
+            this.siyeteBtn.Click += new System.EventHandler(this.siyeteBtn_Click);
             // 
             // otchoBtn
             // 
@@ -197,6 +184,7 @@ namespace Calculator
             this.otchoBtn.TabIndex = 13;
             this.otchoBtn.Text = "8";
             this.otchoBtn.UseVisualStyleBackColor = true;
+            this.otchoBtn.Click += new System.EventHandler(this.otchoBtn_Click);
             // 
             // nuwebeBtn
             // 
@@ -207,17 +195,7 @@ namespace Calculator
             this.nuwebeBtn.TabIndex = 14;
             this.nuwebeBtn.Text = "9";
             this.nuwebeBtn.UseVisualStyleBackColor = true;
-            // 
-            // multiplyBtn
-            // 
-            this.multiplyBtn.BackColor = System.Drawing.Color.PowderBlue;
-            this.multiplyBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.multiplyBtn.Location = new System.Drawing.Point(210, 141);
-            this.multiplyBtn.Name = "multiplyBtn";
-            this.multiplyBtn.Size = new System.Drawing.Size(60, 38);
-            this.multiplyBtn.TabIndex = 15;
-            this.multiplyBtn.Text = "×";
-            this.multiplyBtn.UseVisualStyleBackColor = false;
+            this.nuwebeBtn.Click += new System.EventHandler(this.nuwebeBtn_Click);
             // 
             // deleteBtn
             // 
@@ -229,6 +207,7 @@ namespace Calculator
             this.deleteBtn.TabIndex = 16;
             this.deleteBtn.Text = "→";
             this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
             // 
             // clearBtn
             // 
@@ -240,6 +219,7 @@ namespace Calculator
             this.clearBtn.TabIndex = 17;
             this.clearBtn.Text = "AC";
             this.clearBtn.UseVisualStyleBackColor = false;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
             // 
             // percentBtn
             // 
@@ -251,6 +231,40 @@ namespace Calculator
             this.percentBtn.TabIndex = 18;
             this.percentBtn.Text = "%";
             this.percentBtn.UseVisualStyleBackColor = false;
+            this.percentBtn.Click += new System.EventHandler(this.percentBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.PowderBlue;
+            this.addBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addBtn.Location = new System.Drawing.Point(210, 229);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(60, 38);
+            this.addBtn.TabIndex = 21;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = false;
+            // 
+            // subtractBtn
+            // 
+            this.subtractBtn.BackColor = System.Drawing.Color.PowderBlue;
+            this.subtractBtn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.subtractBtn.Location = new System.Drawing.Point(210, 185);
+            this.subtractBtn.Name = "subtractBtn";
+            this.subtractBtn.Size = new System.Drawing.Size(60, 38);
+            this.subtractBtn.TabIndex = 22;
+            this.subtractBtn.Text = "-";
+            this.subtractBtn.UseVisualStyleBackColor = false;
+            // 
+            // multiplyBtn
+            // 
+            this.multiplyBtn.BackColor = System.Drawing.Color.PowderBlue;
+            this.multiplyBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.multiplyBtn.Location = new System.Drawing.Point(210, 141);
+            this.multiplyBtn.Name = "multiplyBtn";
+            this.multiplyBtn.Size = new System.Drawing.Size(60, 38);
+            this.multiplyBtn.TabIndex = 23;
+            this.multiplyBtn.Text = "×";
+            this.multiplyBtn.UseVisualStyleBackColor = false;
             // 
             // divideBtn
             // 
@@ -259,18 +273,23 @@ namespace Calculator
             this.divideBtn.Location = new System.Drawing.Point(210, 97);
             this.divideBtn.Name = "divideBtn";
             this.divideBtn.Size = new System.Drawing.Size(60, 38);
-            this.divideBtn.TabIndex = 19;
+            this.divideBtn.TabIndex = 24;
             this.divideBtn.Text = "÷";
             this.divideBtn.UseVisualStyleBackColor = false;
             // 
             // programScreen
             // 
             this.programScreen.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.programScreen.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.programScreen.Location = new System.Drawing.Point(12, 12);
             this.programScreen.Multiline = true;
             this.programScreen.Name = "programScreen";
+            this.programScreen.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.programScreen.Size = new System.Drawing.Size(258, 79);
-            this.programScreen.TabIndex = 20;
+            this.programScreen.TabIndex = 26;
+            this.programScreen.Tag = "";
+            this.programScreen.Text = "0";
+            this.programScreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Calculator
             // 
@@ -280,18 +299,18 @@ namespace Calculator
             this.ClientSize = new System.Drawing.Size(282, 360);
             this.Controls.Add(this.programScreen);
             this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.multiplyBtn);
+            this.Controls.Add(this.subtractBtn);
+            this.Controls.Add(this.addBtn);
             this.Controls.Add(this.percentBtn);
             this.Controls.Add(this.clearBtn);
             this.Controls.Add(this.deleteBtn);
-            this.Controls.Add(this.multiplyBtn);
             this.Controls.Add(this.nuwebeBtn);
             this.Controls.Add(this.otchoBtn);
             this.Controls.Add(this.siyeteBtn);
-            this.Controls.Add(this.subtractBtn);
             this.Controls.Add(this.saisBtn);
             this.Controls.Add(this.singkoBtn);
             this.Controls.Add(this.kwatroBtn);
-            this.Controls.Add(this.addBtn);
             this.Controls.Add(this.tresBtn);
             this.Controls.Add(this.dosBtn);
             this.Controls.Add(this.unoBtn);
@@ -300,6 +319,7 @@ namespace Calculator
             this.Controls.Add(this.zeroBtn);
             this.Controls.Add(this.posNegBtn);
             this.Name = "Calculator";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -315,18 +335,18 @@ namespace Calculator
         private System.Windows.Forms.Button unoBtn;
         private System.Windows.Forms.Button dosBtn;
         private System.Windows.Forms.Button tresBtn;
-        private System.Windows.Forms.Button addBtn;
         private System.Windows.Forms.Button kwatroBtn;
         private System.Windows.Forms.Button singkoBtn;
         private System.Windows.Forms.Button saisBtn;
-        private System.Windows.Forms.Button subtractBtn;
         private System.Windows.Forms.Button siyeteBtn;
         private System.Windows.Forms.Button otchoBtn;
         private System.Windows.Forms.Button nuwebeBtn;
-        private System.Windows.Forms.Button multiplyBtn;
         private System.Windows.Forms.Button deleteBtn;
         private System.Windows.Forms.Button clearBtn;
         private System.Windows.Forms.Button percentBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button subtractBtn;
+        private System.Windows.Forms.Button multiplyBtn;
         private System.Windows.Forms.Button divideBtn;
         private System.Windows.Forms.TextBox programScreen;
     }
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -17,9 +17,159 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void zeroBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "0";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "0";
+            }
+        }
+
+        private void unoBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "1";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "1";
+            }
+        }
+
+        private void dosBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "2";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "2";
+            }
+        }
+
+        private void tresBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "3";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "3";
+            }
+        }
+
+        private void kwatroBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "4";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "4";
+            };
+        }
+
+        private void singkoBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "5";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "5";
+            }
+        }
+
+        private void saisBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "6";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "6";
+            }
+        }
+
+        private void siyeteBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "7";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "7";
+            }
+        }
+
+        private void otchoBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "8";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "8";
+            }
+        }
+
+        private void nuwebeBtn_Click(object sender, EventArgs e)
+        {
+            if (programScreen.Text == "0")
+            {
+                programScreen.Text = string.Empty;
+                programScreen.Text = programScreen.Text + "9";
+            }
+            else
+            {
+                programScreen.Text = programScreen.Text + "9";
+            }
+        }
+
+        private void decimalBtn_Click(object sender, EventArgs e)
+        {
+            programScreen.Text = programScreen.Text + ".";
+        }
+
+        private void posNegBtn_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void percentBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            programScreen.Text = string.Empty;
         }
     }
 }
